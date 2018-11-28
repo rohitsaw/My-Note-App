0cce6da22526bc426221e5319bdc97f87a12689a
rohitsaw <rsaw409@gmail.com>
2018-11-28 22:32:38 +0530

    test

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7*n7w1x)e@nv)p*q0=)-5cav(_i3#lcp5iuhs-4i#k3bs697fb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
